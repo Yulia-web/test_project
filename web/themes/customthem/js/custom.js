5f62259c4842dbf8ce1efa72dcd9313f8b93565a
Linzy <lyashchukyulia@gmail.com>
2023-05-09 12:41:47 +0300

    Made corrections for correct display on mobile devices

--- web/themes/customthem/js/custom.js
+++ web/themes/customthem/js/custom.js
@@ -2,7 +2,8 @@
   Drupal.behaviors.portfolioBlock = {
     attach: function (context, settings) {
 
-      $('.view-portfolio-filters > .view-content').once('add-all').prepend('<button class="button" data-filter="*">All</button>');
+      $('.view-portfolio-filters > .view-content').once('add-all').prepend('<button class="button all" data-filter="*">All</button>');
+      $('.view-portfolio-filters button.all').addClass('active');
 
       // init Isotope
       var $grid = $('.view-gallery-front > .view-content').isotope({
@@ -12,8 +13,10 @@
 
       // bind filter button click
       $('.view-portfolio-filters').once().on( 'click', 'button', function() {
+        $('.view-portfolio-filters button').removeClass('active');
         var filterValue = $( this ).attr('data-filter');
         $grid.isotope({ filter: filterValue });
+        $(this).addClass('active');
       });
 
     }
